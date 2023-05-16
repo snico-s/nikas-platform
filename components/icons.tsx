f338baaa43bcf7c8aeebfe942544184d56584be4
Nico <nico.scheideler@gmail.com>
2023-05-16 10:06:41 +0200

    add: signIn and signOut

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,4 +1,5 @@
 import {
+  LogOut,
   LucideProps,
   Menu,
   Moon,
@@ -32,6 +33,7 @@ export const Icons = {
       ></path>
     </svg>
   ),
+  logout: LogOut,
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,23 +1,33 @@
 "use client"
 
-import { useState } from "react"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 import { siteConfig } from "@/config/site"
-import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
+import { MobileNav } from "@/components/mobile-nav"
+import { ThemeToggle } from "@/components/theme-toggle"
 
-import { MobileNav } from "./mobile-nav"
-import { ThemeToggle } from "./theme-toggle"
+import { Icons } from "./icons"
 
 export function SiteHeader() {
-  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false)
+  const { data: session } = useSession()
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className=" flex flex-1 items-center justify-end space-x-4">
-          <nav className="hidden items-center space-x-1 md:flex">
+          <nav className="hidden items-center md:flex">
+            {session ? (
+              <Button variant={"ghost"} size={"sm"} onClick={() => signOut()}>
+                <Icons.logout />
+              </Button>
+            ) : (
+              <Button variant={"outline"} size={"sm"} onClick={() => signIn()}>
+                Login
+              </Button>
+            )}
             <ThemeToggle />
           </nav>
 
